58c53299474e6068a4ed9c94cc9e7ae8849cbf36
developer74 <senior.developer@altama.co.id>
2026-07-28 14:32:10 +0700

    fix

--- app.js
+++ app.js
@@ -22,9 +22,12 @@ app.get('/app2', (req, res) => {
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
-  connection.query(sql,(error, fields) => {    
-    console.log('error', error)
-    res.send(fields)
+  connection.query(sql,(error, fields) => {
+    if (error) {
+      console.log('error', error)
+    } else {
+      res.send(fields)
+    }
   })
 });
 
